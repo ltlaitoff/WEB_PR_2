a65e392aebb01124662da28140056fd4ce4b74dc
Ivan Baluta <ltlaitoff@gmail.com>
2022-09-12 21:43:31 +0300

    chore: Install svg-inline-loader

--- webpack.config.js
+++ webpack.config.js
@@ -27,8 +27,12 @@ module.exports = {
 				use: ['style-loader', 'css-loader', 'sass-loader']
 			},
 			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				test: /\.(png|jpg|jpeg|gif)$/i,
 				type: 'asset/resource'
+			},
+			{
+				test: /\.svg$/,
+				loader: 'svg-inline-loader'
 			}
 		]
 	},
